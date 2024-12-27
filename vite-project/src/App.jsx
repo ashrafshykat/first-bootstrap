b794ba1b6c97d28674da4b0844ad5b5cacd50a01
ashrafshykat <ashrafshykat1@gmail.com>
2024-12-28 04:56:27 +0600

    creating cards using bootstrap

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
 export default function App() {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+        .then((res) => res.json())
+        .then((data) => setUser(data.users))
+  }, [])
+  console.log(users);
   return (
     <>
-      <Button variant="success">Success</Button>
+    {/* <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card> */}
+    <div className="d-flex flex-wrap justify-content-around">
+    {
+      users.map((user) => (
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={user.avatar} />
+          <Card.Body>
+            <Card.Title>{user.name}</Card.Title>
+            <Card.Text>
+              {user.email}
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      ))
+    }
+    </div>
+    
     </>
   )
 }
